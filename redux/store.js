f2824baee0512f68e7aa891f32baa523fa82d2b6
psfr937 <psfr937@gmail.com>
2021-02-22 05:51:42 +0800

    remove redux persist

--- redux/store.js
+++ redux/store.js
@@ -5,44 +5,20 @@ import {createWrapper} from 'next-redux-wrapper';
 import reducer from '../redux/reducers/index';
 import rootSaga from '../redux/actions/index';
 
-const sagaMiddleware = createSagaMiddleware();
 
-const makeConfiguredStore = (reducer) => {
+
+const makeStore = (context) => {
+  // 1: Create the middleware
+  const sagaMiddleware = createSagaMiddleware();
+
   // 2: Add an extra parameter for applying middleware:
   const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
 
   // 3: Run your sagas on server
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
-  return store
-}
-
-const makeStore = (context) => {
-
-  const isServer = typeof window === 'undefined'
-  // 1: Create the middleware
-
-  if(isServer){
-    return makeConfiguredStore(reducer)
-  }
-  else{
-    const {persistStore, persistReducer} = require('redux-persist');
-    const storage = require('redux-persist/lib/storage').default;
-
-    const persistConfig = {
-      key: 'nextjs',
-      whitelist: ['oneInventory'], // make sure it does not clash with server keys
-      storage
-    };
-    let persistedReducer = persistReducer(persistConfig, reducer);
-
-    const store = makeConfiguredStore(persistedReducer);
-
-    store.__persistor = persistStore(store); // Nasty hack
-
-    return store;
-  }
-
+  // 4: now return the store:
+  return store;
 };
 
 export const wrapper = createWrapper(makeStore);
