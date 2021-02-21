981800f3b273d8b29d8e417cdd9535f4e1d56206
psfr937 <psfr937@gmail.com>
2021-02-22 01:34:15 +0800

    redux persist seems to work and does not break saga

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,21 @@ import "react-vis/dist/style.css";
 import "../styles/checkout.css"
 import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import {wrapper} from '../redux/store';
+import { useStore } from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react';
 
 
 
 const WrappedApp = (ctx) => {
     console.log(ctx)
     const {Component, pageProps, router} = ctx
+    const store = useStore();
     return(
-      <AnimateSharedLayout>
-        <Component {...pageProps} key={router.route}/>
-      </AnimateSharedLayout>
+      <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+          <AnimateSharedLayout>
+            <Component {...pageProps} key={router.route}/>
+          </AnimateSharedLayout>
+      </PersistGate>
     )
 };
 
--- redux/store.js
+++ redux/store.js
@@ -5,20 +5,44 @@ import {createWrapper} from 'next-redux-wrapper';
 import reducer from '../redux/reducers/index';
 import rootSaga from '../redux/actions/index';
 
+const sagaMiddleware = createSagaMiddleware();
 
-
-const makeStore = (context) => {
-  // 1: Create the middleware
-  const sagaMiddleware = createSagaMiddleware();
-
+const makeConfiguredStore = (reducer) => {
   // 2: Add an extra parameter for applying middleware:
   const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
 
   // 3: Run your sagas on server
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
-  // 4: now return the store:
-  return store;
+  return store
+}
+
+const makeStore = (context) => {
+
+  const isServer = typeof window === 'undefined'
+  // 1: Create the middleware
+
+  if(isServer){
+    return makeConfiguredStore(reducer)
+  }
+  else{
+    const {persistStore, persistReducer} = require('redux-persist');
+    const storage = require('redux-persist/lib/storage').default;
+
+    const persistConfig = {
+      key: 'nextjs',
+      whitelist: ['auth'], // make sure it does not clash with server keys
+      storage
+    };
+    let persistedReducer = persistReducer(persistConfig, reducer);
+
+    const store = makeConfiguredStore(persistedReducer);
+
+    store.__persistor = persistStore(store); // Nasty hack
+
+    return store;
+  }
+
 };
 
 export const wrapper = createWrapper(makeStore);
